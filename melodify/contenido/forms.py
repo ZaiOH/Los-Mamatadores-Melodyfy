28ced6e08b110b8a293cf699030c6e26d205aa2b
Juan Carlos Zenteno Pompa <juancarlos_zp@ciencias.unam.mx>
2025-11-04 16:33:07 -0600

    Trabajo intermedio de Listas de REproducción. No se puede seguir hasta que se implementen inicios de sesión, reproducción de audio y búsquedas

---
+++ melodify/contenido/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LDRNombreForm(forms.Form):
+    nuevo_nombre = forms.CharField("Nuevo Nombre de la Lista", max_length=100)
+
--- melodify/contenido/views.py
+++ melodify/contenido/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from contenido.models import LDR
+from .models import LDR
 
 class VistaPrincipal(TemplateView):
     template_name = "base.html"
 
-@login_required
-def ver_ldr(request):
-    return redirect("")
+def ver_ldr(request, ldr_id=None):
+    ldr = get_object_or_404(LDR, id=ldr_id)
+
+    #TODO: checar que es editor
+    if request.method == "POST" and "renombre" in request.POST:
+        ldr.nombre = request.POST["renombre"]
+        ldr.save()
+        return redirect("ver-ldr", ldr_id=ldr_id)
+    canciones = ldr.canciones.all()
+    is_editor = False#ldr.editores.contains(request.user)
+
+    context = {
+        'id': ldr_id,
+        'is_editor': is_editor,
+        'nombre': ldr.nombre,
+        'canciones': canciones
+    }
+
+    return render(request, 'contenido/ldr.html', context)
+
 @login_required
 def crear_ldr(request):
     ldr = LDR(nombre="Nueva lista de reproduccion")
